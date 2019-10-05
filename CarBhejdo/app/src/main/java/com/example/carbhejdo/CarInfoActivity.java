af8f4a9a94267c19b8cf9b353f6f474b45118bfc
bharadwaj1995 <46728675+bharadwaj1995@users.noreply.github.com>
2019-10-05 01:55:17 -0500

    adding intent activity after posing car for selling

--- CarBhejdo/app/src/main/java/com/example/carbhejdo/CarInfoActivity.java
+++ CarBhejdo/app/src/main/java/com/example/carbhejdo/CarInfoActivity.java
@@ -2,7 +2,9 @@ package com.example.carbhejdo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CarInfoActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class CarInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_info);
+    }
+
+    public void after_posting_car_for_selling(View v){
+        Intent ini = new Intent(this, MainScreen.class);
+        startActivity(ini);
     }
 }
