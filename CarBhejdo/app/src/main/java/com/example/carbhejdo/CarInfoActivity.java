5cf1f39fd65a355d56548a06cf9572786088441a
Nandini Yadav Janga <yadavnandini97@gmail.com>
2019-10-26 15:53:38 -0500

    Added stubs for how to add images to the application

--- CarBhejdo/app/src/main/java/com/example/carbhejdo/CarInfoActivity.java
+++ CarBhejdo/app/src/main/java/com/example/carbhejdo/CarInfoActivity.java
@@ -1,21 +1,54 @@
 package com.example.carbhejdo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class CarInfoActivity extends AppCompatActivity {
+    ImageView addimage;
+    Button addimgbutton;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_info);
+         addimage = findViewById(R.id.addimage);
+        addimgbutton = findViewById(R.id.addimgbutton);
+        addimgbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery();
+            }
+        });
+
     }
 
     public void onPostClick(View v){
         Intent ini = new Intent(this, BuyCarListActivity.class);
         startActivity(ini);
+    }
+
+private void openGallery(){
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery,PICK_IMAGE);
+}
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            addimage.setImageURI(imageUri);
+
+        }
     }
 }
