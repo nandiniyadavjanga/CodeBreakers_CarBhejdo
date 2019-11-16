efc7eba4494f8767b0857b2b5a8290d45746a8cb
Nandini Yadav Janga <yadavnandini97@gmail.com>
2019-11-16 00:41:26 -0600

    updated navigation functionality

--- CarBhejdo/app/src/main/java/com/example/carbhejdo/Singlecar_Activity.java
+++ CarBhejdo/app/src/main/java/com/example/carbhejdo/Singlecar_Activity.java
@@ -1,31 +1,65 @@
 package com.example.carbhejdo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
-public class Singlecar_Activity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class Singlecar_Activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singlecar_);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view1);
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     public void onFilterClick(View v){
         Intent filterini = new Intent(this,FilterActivity.class);
         startActivity(filterini);
     }
-//
-//    public void onLogoClick(View v){
-//        Intent logoini = new Intent(this,MenuActivity.class);
-//        startActivity(logoini);
-//    }
+
 
     public void sellerprof(View view) {
         Intent sprof = new Intent(this,sellerprof.class);
         startActivity(sprof);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        if(id==R.id.profile){
+            Intent ini = new Intent(this,sellerprof.class);
+            startActivity(ini);
+            //Toast.makeText(this,"This is profile",Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
 }
+
+
+
